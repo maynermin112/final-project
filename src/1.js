0e7351d3f1f985b200b1169d284737454ace5d01
maynermin112 <maynermin112@gmail.com>
2025-03-06 08:22:14 +0000

    Add 1.js

---
+++ src/1.js
@@ -0,0 +1,8 @@
+function getRandomNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// usage example:
+console.log(getRandomNumber(5, 10));
