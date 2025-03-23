61132890dc6fd0ae40962f6308b54a703402dcf4
maynermin112 <maynermin112@gmail.com>
2025-03-23 06:32:04 +0000

    Add 17.js

---
+++ src/17.js
@@ -0,0 +1,15 @@
+function isPrime(num) {
+  if (num <= 1) {
+    return false;
+  }
+  
+  for (let i = 2; i * i <= num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  
+  return true;
+}
+
+console.log(isPrime(37)); // Output: true
