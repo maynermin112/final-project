ca647023dab03df1ef93712ec603680f609ef402
maynermin112 <maynermin112@gmail.com>
2025-04-07 20:56:22 +0000

    Add 32.js

---
+++ src/32.js
@@ -0,0 +1,7 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+// Example usage:
+const max = 10;
+console.log(getRandomInt(max)); // Output: a random integer between 0 and 9
